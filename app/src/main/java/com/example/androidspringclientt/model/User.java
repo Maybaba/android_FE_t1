6a6f415e8b0654113f707a044afcd5c8de3fa9d3
Maybaba <sinyunjong@gmail.com>
2024-09-26 11:10:53 +0900

    # set activity_main.xml

---
+++ app/src/main/java/com/example/androidspringclientt/model/User.java
@@ -0,0 +1,47 @@
+package com.example.androidspringclientt.model;
+
+public class User {
+    private Integer userIdxId;
+    private String userEmail;
+    private String userPassword;
+    private String userName;
+
+    public Integer getUserIdxId() {
+        return userIdxId;
+    }
+
+    public void setUserIdxId(Integer userIdxId) {
+        this.userIdxId = userIdxId;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public User(Integer userIdxId, String userEmail, String userPassword, String userName) {
+        this.userIdxId = userIdxId;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.userName = userName;
+    }
+}
---
+++ app/src/main/java/com/example/androidspringclientt/retrofit/UserApi.java
@@ -0,0 +1,19 @@
+package com.example.androidspringclientt.retrofit;
+
+import com.example.androidspringclientt.model.User;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface UserApi {
+    @GET("/user/getAllUsers")
+    Call<List<User>> getAllUsers();
+
+    @POST("/user/save")
+    Call<User> save(@Body User user);
+
+}
